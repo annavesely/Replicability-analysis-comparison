a43b91fb7c9cf86fb7435661901c3f367a2e145c
Anna Vesely <53512166+annavesely@users.noreply.github.com>
2026-03-23 15:45:44 +0100

    upload main code

---
+++ code.R
@@ -0,0 +1,115 @@
+
+require(hommel)
+require(adaFilter)
+
+
+# -------------------------------------------------------
+
+
+# Unconditional PC p-value through Fisher combination
+# x: vector of sorted p-values
+# gamma: replicability level
+# output: PC p-value
+
+poolPvals <- function(x, gamma){
+  z <- x[gamma : length(x)]
+  comb <- -2*sum(log(z))
+  out <- pchisq(comb, df=length(z)*2, lower.tail=FALSE)
+  return(out)
+}
+
+
+# -------------------------------------------------------
+
+
+# Benjamini, Heller, Yekutieli (2009)
+# pmat0: matrix of pvalues (m rows = features, s columns = subjects);
+# alpha: significance level
+# output: boolean vector of length m, keeping track of replicability
+
+BHY <- function(pmat0, alpha=0.05){
+  
+  pmat <- t(apply(pmat0, 1, function(x) sort(x)))
+  m <- nrow(pmat)
+  s <- ncol(pmat)
+  
+  X <- rep(0,m)
+  
+  pv <- apply(pmat, 1, function(x) poolPvals(x, 1))
+  tmp <- p.adjust(pv, method="BH")
+  sel <- which(tmp <= alpha)
+  m1 <- length(sel)
+  thr <- alpha * m1 / m
+  
+  for(j in sel){
+    gamma <- 0
+    
+    while(gamma < s){
+      
+      gamma <- gamma + 1
+      
+      pv <- poolPvals(pmat[j,], gamma)
+      if(pv > thr){
+        gamma <- gamma - 1
+        break
+      }
+    }
+    X[j] <- gamma
+  }
+  
+  return(X)
+}
+
+
+# -------------------------------------------------------
+
+
+# adaFilter: Wang, Gui, Su, Sabatti, Owen (2020)
+# pmat0: matrix of pvalues (m rows = features, s columns = subjects);
+# alpha: significance level
+# output: boolean vector of length m, keeping track of replicability
+
+adafilter <- function(pmat0, alpha=0.05){
+  
+  m <- nrow(pmat0)
+  s <- ncol(pmat0)
+  
+  X <- rep(0,m)
+  
+  for(gamma in (2:s)){
+    rej <- adaFilter(pmat0, r=gamma, type.I.err="FDR", alpha=alpha)$decision
+    rej <- as.logical(rej)
+    X[rej] <- gamma
+  }
+  
+  return(X)
+}
+
+
+# -------------------------------------------------------
+
+
+# ARI: Goeman and Solari (2011)
+# pmat0: matrix of pvalues (m rows = features, s columns = subjects);
+# alpha: significance level
+# output: boolean vector of length m, keeping track of replicability
+
+ari <- function(pmat0, alpha=0.05){
+  
+  m <- nrow(pmat0)
+  s <- ncol(pmat0)
+  
+  p <- as.vector(pmat0)
+  hom <- hommel::hommel(p)
+  
+  out <- rep(0,m)
+  
+  for(j in 1:m){
+    ix <- (0:(s-1))*m + j
+    out[j] <- hommel::discoveries(hom, ix=ix, alpha=alpha)
+  }
+  
+  return(out)
+}
+
+
---
+++ gene_data.R
@@ -0,0 +1,85 @@
+
+require(curatedOvarianData)
+require(KEGGREST)
+
+
+# -------------------------------------------------------
+
+
+# P-values from single study
+# eset: gene expression set with m genes
+# output: vector of length m containing p-values for each gene
+
+compute_pvals <- function(eset){
+  
+  group <- eset$sample_type
+  group <- ifelse(group %in% c("tumor", "metastatic"), "tumor", "healthy")
+  
+  expr <- exprs(eset)
+  
+  out <- apply(expr, 1, function(x) t.test(x ~ group)$p.value)
+  return(out)
+}
+
+
+# -------------------------------------------------------
+
+
+# KEGG pathways
+# pathway_id: identification number of the pathway
+# output: vector of genes inside the pathway
+
+get_kegg_genes <- function(pathway_id) {
+  
+  pathway <- KEGGREST::keggGet(pathway_id)[[1]]
+  out <- sapply(pathway$GENE, function(x) x[1])
+  out <- sub(";.*", "", out)
+  names(out) <- NULL
+  
+  return(out)
+}
+
+
+# -------------------------------------------------------
+
+data("GSE18520_eset")
+data("GSE6008_eset")
+data("GSE26712_eset")
+data("GSE9891_eset")
+data("GSE51088_eset")
+
+pvals1 <- compute_pvals(GSE18520_eset)
+pvals2 <- compute_pvals(GSE6008_eset)
+pvals3 <- compute_pvals(GSE26712_eset)
+pvals4 <- compute_pvals(GSE9891_eset)
+pvals5 <- compute_pvals(GSE51088_eset)
+
+
+common_genes <- Reduce(intersect, list(
+  names(pvals1), names(pvals2), names(pvals3), names(pvals4), names(pvals5)
+))
+
+
+pmat0 <- cbind(
+  pvals1[common_genes],
+  pvals2[common_genes],
+  pvals3[common_genes],
+  pvals4[common_genes],
+  pvals5[common_genes]
+)
+
+
+# -------------------------------------------------------
+
+
+# PI3K-Akt signaling
+pi3k_genes <- get_kegg_genes("hsa04151")
+
+# p53 signaling
+p53_genes <- get_kegg_genes("hsa04115")
+
+
+# -------------------------------------------------------
+
+
+save(pmat0, pi3k_genes, p53_genes, file="ovarian_cancer_data.RData")
